7a4d52bb02eb3eb2ddbfd03cd5cb3042d8970775
Jakob Mifflin <jmiffli@siue.edu>
2026-02-26 19:42:29 -0600

    Fixed All the issues

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class FollowCam : MonoBehaviour
 {
     static public GameObject POI; // The static point of interest
+    [Header("Inscribed")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero; // [0,0]
 
     [Header("Dynamic")]
     public float camZ; //The desired Z pos of the camera
@@ -16,14 +19,34 @@ public class FollowCam : MonoBehaviour
 
     void FixedUpdate()
     {
+        Vector3 destination = Vector3.zero;
+        if (POI != null)
+        {
+            //If the POI has a Rigidbody, check to see if it is sleeping
+            Rigidbody poiRigid = POI.GetComponent<Rigidbody>();
+            if ((poiRigid != null) && poiRigid.IsSleeping())
+            {
+                POI = null;
+            }
+        }
+        if (POI != null)
+        {
+            destination = POI.transform.position;
+        }
         //A single line if statement doesn't require braces
-        if (POI == null) return; // of there is no POI, then return
-
+        //if (POI == null) return; // of there is no POI, then return
         //Get position of the poi
-        Vector3 destination = POI.transform.position;
+        //Vector3 destination = POI.transform.position;
+        //Limit the minimum values of destniation.x and destination.y
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        //Interpolate from the current Camera position toward destination
+        destination = Vector3.Lerp(transform.position, destination, easing);
         //Force Destination.z to be camZ to keep the camera far enough away
         destination.z = camZ;
         //Set the camera to the destination
         transform.position = destination;
+        //Set the orthographicSize of the camera to keep ground in view
+        Camera.main.orthographicSize = destination.y + 10;
     }
 }
